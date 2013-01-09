d730edcf3ef25e3ea10498d2a6f7264cd87ceedb
Honghua Zhu <zhh200910@gmail.com>
2013-01-09 20:50:31 +0800

    删除不再使用的文件

--- douyu-examples/WEB-INF/src/FormExample.java
+++
@@ -1,7 +0,0 @@
-@douyu.mvc.Controller
-public class FormExample {
-	public void save(String name, int age, java.io.PrintWriter out) {
-		out.println("name = " + name);
-		out.println("age  = " + age);
-	}
-}
